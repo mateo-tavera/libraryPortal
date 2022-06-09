59de370ca2f8eaff1640a3cd7f355488cbde5906
Mateo Tavera <mateo.tavera8@gmail.com>
2022-06-08 19:24:02 -0500

    Write comments in main.go and execute go mod tidy

--- go.mod
+++ go.mod
@@ -6,10 +6,12 @@ require github.com/gorilla/mux v1.8.0
 
 require github.com/gorilla/schema v1.2.0
 
-require github.com/google/uuid v1.3.0
+require (
+	github.com/google/uuid v1.3.0
+	github.com/labstack/echo v3.3.10+incompatible
+)
 
 require (
-	github.com/labstack/echo v3.3.10+incompatible // indirect
 	github.com/labstack/gommon v0.3.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
--- main.go
+++ main.go
@@ -13,8 +13,10 @@ import (
 
 func main() {
 
+	//Using the command line to control the execution
 	framework := string(os.Args[1])
 
+	//Switch structure to select which framework execute
 	switch framework {
 
 	case "gorilla":
@@ -27,13 +29,24 @@ func main() {
 		a.RegisterRoutes(r)
 
 		//Start server
+		fmt.Println("Using Gorilla/Mux")
 		fmt.Println("Listening...")
 		http.ListenAndServe(":8081", r)
+
 	case "echo":
+		//Create the rout pointer
 		e := echo.New()
+
+		//Create the api object
 		a := &apiEcho.API{}
+
+		//Resgiter the routes to that api
 		a.RegisterRoutes(e)
+
+		//Start server
+		fmt.Println("Using Echo")
 		e.Logger.Fatal(e.Start(":8081"))
+
 	default:
 		fmt.Println("No framework selected. App cannot start")
 	}
